97a6f966075c5dde8ee06dbf499b655ec3b62e87
jp2268 <jp2268@cornell.edu>
2019-02-12 15:28:07 -0500

    migrated blog content and changed the p5 stuff

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var coords = [40, 40, 80, 60, 100, 100, 60, 120, 50, 150];
+
+setup() {
+    createCanvas(1000, 1000);
+    y = random();
+}
+draw() {
+    background(255);
+    noFill();
+    stroke(0);
+
+    y += random()/100
+    // y -= random()
+    // y--
+    // y++
+    for (let x=0; x<y; x++) {
+        beginShape();
+        curveVertex(40+x*10*y,40);
+        curveVertex(40+x*10*y,40);
+        curveVertex(80+x*10*y,60);
+        curveVertex(100,100);
+        curveVertex(60*y,120);
+        curveVertex(50*y,150);
+        curveVertex(50,150);
+        curveVertex(300,300);
+        curveVertex(1000,1000);
+        endShape();
+    } 
+}
